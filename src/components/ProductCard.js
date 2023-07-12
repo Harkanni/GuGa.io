5ace7033ce5e936397bc1b179e4628f68243d266
harkanni <harkanzyempire001@gmail.com>
2023-07-12 08:23:55 +0100

    Responsive Product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,14 +4,15 @@ import { Button } from './Button';
 export const ProductCard = (props) => {
   return (
     <div className={styles.premium}>
-      <h2>{props.type}</h2>
-      <h3>{ props.h1 }</h3>
-      <p>
-        {props.p}
-      </p>
-      <Button text={props.text} specialStyle={props.btnBgColour}></Button>
+      <div className={styles.text}>
+        <h2>{props.type}</h2>
+        <h3>{props.h1}</h3>
+        <p>{props.p}</p>
+        <Button text={props.text} specialStyle={props.btnBgColour}></Button>
+      </div>
+
       <div className={styles.imageContainer}>
-        <img src={ props.imgUrl } alt="" />
+        <img src={props.imgUrl} alt='' />
       </div>
     </div>
   );
